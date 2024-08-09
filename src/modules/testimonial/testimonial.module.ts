d53048ab3ed3b75006f83deafaa91fd930d3a69d
Spyke Lionel <social.lionel@gmail.com>
2024-08-09 16:24:09 +0100

    remove unused

--- src/modules/testimonial/testimonial.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { TestimonialService } from './testimonial.service';
-import { TestimonialController } from './testimonial.controller';
-
-@Module({
-  controllers: [TestimonialController],
-  providers: [TestimonialService],
-})
-export class TestimonialModule {}
